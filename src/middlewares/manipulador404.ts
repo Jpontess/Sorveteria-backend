700af0836f78c14fe225a38d183fd3e7c6503ba0
joao pontes <Joo>
2026-04-28 21:49:20 -0300

    update

--- src/middlewares/manipulador404.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'express';
-
-function manipulador404 (req: Request, res: Response){
-  res.status(404).send({
-    mensagem: 'Erro: Página não encontrada.'
-  });
-}
-
-export default manipulador404;
